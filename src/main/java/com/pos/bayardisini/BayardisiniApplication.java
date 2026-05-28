451b632e27697fb680aa66e9543480e76f4bf758
rezaalkhoeri <rezaalkhoeri2403@gmail.com>
2026-05-29 01:28:33 +0700

    Initialize Spring Boot project with Maven wrapper, application properties, and basic structure

---
+++ src/main/java/com/pos/bayardisini/BayardisiniApplication.java
@@ -0,0 +1,13 @@
+package com.pos.bayardisini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BayardisiniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BayardisiniApplication.class, args);
+	}
+
+}
